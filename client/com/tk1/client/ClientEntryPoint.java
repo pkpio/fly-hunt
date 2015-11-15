aec09eab1e3f11f256c6e65cb969a28af536c109
rkamath3 <aramachandrakamath@gmail.com>
2015-11-15 12:17:38 +0100

    some refcatoring

--- client/com/tk1/client/ClientEntryPoint.java
+++ client/com/tk1/client/ClientEntryPoint.java
@@ -2,27 +2,28 @@ package com.tk1.client;
 
 import java.rmi.RemoteException;
 import java.util.ArrayList;
-import java.util.Random;
 
 import com.tk1.player.Player;
 
 public class ClientEntryPoint {
-
-	private static final long serialVersionUID = 1L;
+	private Player player;
+	private GameFrame gameFrame;
 
 	protected ClientEntryPoint() throws RemoteException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	private Player playerObj;
-	private GameFrame clientViewObj;
+	public static void main(String[] args) throws RemoteException {
+		new ClientEntryPoint().initGame("");
+
+	}
 
 	void initGame(String port) throws RemoteException {
-		clientViewObj = new GameFrame();
-		playerObj = new Player();
-		clientViewObj.setLoginUI();
-		clientViewObj.addListener(new ClientViewEvent() {
+		gameFrame = new GameFrame();
+		player = new Player();
+		gameFrame.setLoginUI();
+		gameFrame.addListener(new ClientViewEvent() {
 			@Override
 			public void flieHunted() {
 
@@ -32,11 +33,11 @@ public class ClientEntryPoint {
 			public void playerLogin(String name) {
 
 				boolean status;
-				playerObj.setPlayerName(name);
+				player.setPlayerName(name);
 				try {
 					status = true;// addServerIntf.login(name);
 					if (status) {
-						clientViewObj.init();
+						gameFrame.init();
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -51,20 +52,14 @@ public class ClientEntryPoint {
 
 	}
 
-	public static void main(String[] args) throws RemoteException {
-		new ClientEntryPoint().initGame("");
-
-	}
-
-
 	public void updatePlayerInfo(ArrayList<Player> playerList) {
 
-		String playerListString = new String("Hello  " + playerObj.getPlayerName() + "\n\n");
+		String playerListString = new String("Hello  " + player.getPlayerName() + "\n\n");
 		playerListString += "Player   \tScore" + "\n";
 		for (Player P : playerList) {
 			playerListString += P.getPlayerName() + "\t" + String.valueOf(P.getPoints()) + "\n";
 		}
-		clientViewObj.updatePlayerList(playerListString);
+		gameFrame.updatePlayerList(playerListString);
 	}
 
 }
